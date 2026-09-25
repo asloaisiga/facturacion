254732ffea8e562bcda031b257bf35e8ffbad7b0
Alexa Loaisiga <asloaisiga@uamv.edu.ni>
2026-09-25 15:58:48 -0600

    update pom, module y conexion

--- facturacion/src/main/java/module-info.java
+++ facturacion/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module ni.edu.uam.facturacion {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.postgresql.jdbc;
 
 
     opens ni.edu.uam.facturacion to javafx.fxml;
--- facturacion/src/main/java/ni/edu/uam/facturacion/database/ConexionDB.java
+++ facturacion/src/main/java/ni/edu/uam/facturacion/database/ConexionDB.java
@@ -1,4 +1,32 @@
 package ni.edu.uam.facturacion.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/tienda_javafx";
+
+    private static final String USUARIO = "postgres";
+
+    private static final String PASSWORD = "TU_CONTRASEÑA";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+
+        try (Connection connection = getConnection()) {
+
+            System.out.println("Conexión exitosa con PostgreSQL");
+
+        } catch (SQLException e) {
+
+            System.out.println("Error al conectar con PostgreSQL");
+            System.out.println(e.getMessage());
+        }
+    }
 }
