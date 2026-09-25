6b64b186f65f8f8c5a4bd42e1b8dcb30e0e0fd7c
Alexa Loaisiga <asloaisiga@uamv.edu.ni>
2026-09-25 16:17:23 -0600

    Finish FacturacionApp

--- facturacion/src/main/java/ni/edu/uam/facturacion/FacturacionApplication.java
+++ facturacion/src/main/java/ni/edu/uam/facturacion/FacturacionApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class FacturacionApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FacturacionApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(FacturacionApplication.class.getResource("menu-principal.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Facturación APP");
         stage.setScene(scene);
         stage.show();
     }
